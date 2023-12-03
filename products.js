8dc42cdfb190ba6b3d1c4f040880f1cbc95d641e
MarcosVinicius1990 <pereiradasilva@yahoo.com.br>
2023-12-03 16:53:57 -0300

    Projeto-Treino-JS

---
+++ products.js
@@ -0,0 +1,9 @@
+const menuOptions = [
+    { name: 'X-salada', price: 30, vegan: false, src: './img/x-salada.jpg'},
+    { name: 'X-Bacon', price: 34, vegan: false, src: './img/x-bacon.jpg'},
+    { name: 'X-Bacon Egg', price: 39, vegan: false, src: './img/x-egg.jpg'},
+    { name: 'Monstruoso', price: 50, vegan: false, src: './img/x-monstro.jpg'},
+    { name: 'Big Vegano', price: 55, vegan: true, src: './img/x-monstro-vegano.jpg'},
+    { name: 'X-Vegan', price: 45, vegan: true, src: './img/x-vegano.jpg'},
+]
+
